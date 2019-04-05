a39bb021226652f8071062461e787ff180445498
arjang <arjangt@yahoo.com>
2019-04-05 21:58:35 +0100

    saving progress

--- src/graph_elements.c
+++ src/graph_elements.c
@@ -18,7 +18,8 @@
 #include "prime.h"
 
 // HT_DELETED_ITEM is used to mark a bucket containing a deleted item
-static neighbour DELETED_NEIGHBOUR = {NULL, NULL};
+static edge DELETED_EDGE = {NULL, NULL};
+static edges DELETED_EDGES = {NULL, {NULL, NULL}};
 static node DELETED_NODE = {NULL, NULL};
 
 // PRIMEs are parameters in the hashing algorithm
@@ -26,10 +27,10 @@ static const int PRIME_1 = 151;
 static const int PRIME_2 = 163;
 static const int INITIAL_BASE_SIZE = 0;
 
-// Define initialization functions for `node`s and `neighbour`s.
+// Define initialization functions for `node`s and `edge`s.
 // This function allocates a chunk of memory the size
-// of an `neighbour`, and saves a copy of the `key` and
-// float `distance`for neighbour, and `key` and `location` for the node,
+// of an `edge`, and saves a copy of the `key` and
+// float `distance`for edge, and `key` and `location` for the node,
 // in the new chunk of memory. The function is marked as
 // `static` because it will only ever be called by code internal to the
 // graph.
@@ -41,8 +42,8 @@ static node* new_node(const char* key, gps* location) {
     return n;
 }
 
-static neighbour* new_neighbour(const char* node, float* distance) {
-    neighbour* n = malloc(sizeof(neighbour));
+static edge* new_edge(const char* node, float* distance) {
+    edge* n = malloc(sizeof(edge));
     n->node = strdup(node);
     n->distance = distance;
     return n;
@@ -50,46 +51,47 @@ static neighbour* new_neighbour(const char* node, float* distance) {
 
 // `create_nodes` and `create_edge` initializes a new hash table.
 // `size` defines how many nodes and edges we can store for each,
-// fixed at 53 for now. Initialize the array of nodes and neighbours
+// fixed at 53 for now. Initialize the array of nodes and edges
 // with `calloc`, which fills the allocated memory with `NULL`
 // bytes. A `NULL` entry in the array indicates that the bucket is empty.
 // Support creating a hash table of a certain size. To do this,
 // `create_edges_sized` and `create_nodes_sized` are called by `create_edges`
 // and `create_nodes`, respectively.
-static edges_table* create_edges_sized(const int size_index) {
-    edges_table* E = xmalloc(sizeof(edges_table));
-    E->size_index = size_index;
-    
-    const int base_size = 50 << E->size_index;
-    E->size = next_prime(base_size);
-    
-    E->count = 0;
-    E->neighbours = xcalloc((size_t)E->size, sizeof(neighbours*));
-    return E;
+
+edgeList* create_edges_list(char* n) {
+	edgeList* newEdgeList;
+	newEdgeList = (edgeList *) malloc (sizeof(edgeList));
+	newEdgeList->node = (char *)malloc(strlen(n) + 1);
+    strcpy(newEdgeList->node, n);
+    newEdgeList->next = NULL;
+	return newEdgeList;
 }
 
-neighbours* create_neighbours(const int size_index) {
-    neighbours* n = xmalloc(sizeof(neighbours));
-    if (n != NULL) {
-        const int base_size = 50 << n->size_index;
-        n->size = next_prime(base_size);
+
+edges* create_edges_sized(const int size_index) {
+    edges* E = xmalloc(sizeof(edges));
+    if (E != NULL) {
+        const int base_size = 50 << E->size_index;
+        E->size = next_prime(base_size);
         
-        n->count = 0;
-        n->neighbours = xcalloc((size_t)n->size, sizeof(neighbours*));
-        if (n->neighbours == NULL) {
-            free(n);
+        E->esdgesStart = NULL;
+        E->count = 0;
+        E->esdgesStart;
+        E->edges = xcalloc((size_t)E->size, sizeof(edges*));
+        if (E->edges == NULL) {
+            free(E);
             return NULL;
         }
     }
-    return n;
+    return E;
 }
 
-edges_table* create_edges() {
+edges* create_edges() {
     return create_edges_sized(INITIAL_BASE_SIZE);
 }
 
-static nodes_table* create_nodes_sized(const int size_index) {
-    nodes_table* N = xmalloc(sizeof(nodes_table));
+static nodes* create_nodes_sized(const int size_index) {
+    nodes* N = xmalloc(sizeof(nodes));
     N->size_index = size_index;
     
     const int base_size = 50 << N->size_index;
@@ -100,17 +102,17 @@ static nodes_table* create_nodes_sized(const int size_index) {
     return N;
 }
 
-nodes_table* create_nodes() {
+nodes* create_nodes() {
     return create_nodes_sized(INITIAL_BASE_SIZE);
 }
 
 // Create a graph from the nodes and edges tables.
 graph* create_graph() {
-    edges_table* E = create_edges();
-    nodes_table* N = create_nodes();
+    edges* E = create_edges_table();
+    nodes* N = create_nodes();
     graph* G = xmalloc(sizeof(graph));
-    G->N = *N;
-    G->E = *E;
+    G->N = N;
+    G->E = E;
     return G;
 }
 
@@ -119,14 +121,14 @@ graph* create_graph() {
 // Initialize new hash table with desired size. All non-`NULL` or
 // deleted items are inserted into the new hash table. Then swap
 // attributes of the new and old hash tables before deleting the latter.
-static void resize_nodes(nodes_table* N, const int direction) {
+static void resize_nodes(nodes* N, const int direction) {
     const int new_size_index = N->size_index + direction;
     if (new_size_index < INITIAL_BASE_SIZE) {
         // Don't resize down the smallest hash table
         return;
     }
     // Create a temporary new hash table to insert items into
-    nodes_table* new_N = create_nodes_sized(new_size_index);
+    nodes* new_N = create_nodes_sized(new_size_index);
     // Iterate through existing hash table, add all items to new
     for (int i = 0; i < N->size; i++) {
         node* node = N->nodes[i];
@@ -151,19 +153,45 @@ static void resize_nodes(nodes_table* N, const int direction) {
     delete_nodes(new_N);
 }
 
-static void resize_edges(edges_table* E, const int direction) {
+static void resize_edges(edges* N, const int direction) {
+    const int new_size_index = N->size_index + direction;
+    if (new_size_index < INITIAL_BASE_SIZE) {
+        return;
+    }
+    edges* new_N = create_edges(new_size_index);
+    for (int i = 0; i < N->size; i++) {
+        edge* n = N->edges[i];
+        if (n != NULL && n != &DELETED_EDGE) {
+            add_edge(N, n);
+        }
+    }
+    N->size = new_N->size;
+    N->count = new_N->count;
+    
+    const int tmp_size = N->size;
+    N->size = new_N->size;
+    new_N->size = tmp_size;
+    
+    edge** tmp_edges = N->edges;
+    N->edges = new_N->edges;
+    new_N->edges = tmp_edges;
+    
+    delete_edges(new_N);
+}
+
+static void resize_edges(edges* E, const int direction) {
     const int new_size_index = E->size_index + direction;
     if (new_size_index < INITIAL_BASE_SIZE) {
         // Don't resize down the smallest hash table
         return;
     }
     // Create a temporary new hash table to insert items into
-    edges_table* new_E = create_edges_sized(new_size_index);
+    edges* new_E = create_edges_sized(new_size_index);
     // Iterate through existing hash table, add all items to new
     for (int i = 0; i < E->size; i++) {
-        neighbours* n = E->neighbours[i];
-        if (n != NULL && n != &DELETED_NEIGHBOUR) {
-            add_edge(E, n);
+        edges* n = E->edges[i];
+        if (n != NULL && n != &DELETED_EDGES) {
+            add_edges(E, n);
         }
     }
     
@@ -176,19 +204,14 @@ static void resize_edges(edges_table* E, const int direction) {
     E->size = new_E->size;
     new_E->size = tmp_size;
     
-    neighbour** tmp_neighbours = E->neighbours;
-    E->neighbours = new_E->neighbours;
-    new_E->neighbours = tmp_neighbours;
+    edges** tmp_edges = E->edges;
+    E->edges = new_E->edges;
+    new_E->edges = tmp_edges;
     
-    delete_edges(new_E);
+    delete_edges_table(new_E);
 }
 
-// Deleting neighbour and edges table
-static void delete_neighbour(neighbour* n) {
-    free(n->node);
-    free(n->distance);
-    free(n);
-}
+// Deleting edge and edges table
 
 static void delete_node(node* n) {
     gps* location = n->location;
@@ -199,19 +222,41 @@ static void delete_node(node* n) {
     free(n);
 }
 
-void delete_edges(edges_table* E) {
-    for (int i = 0; i < E->size; i++) {
-        neighbours* n = E->neighbours[i];
-        if (n != NULL && n != &DELETED_NEIGHBOUR) {
-            delete_neighbour(n);
+
+void delete_edge(edges* N, edge* n) {
+    for (int i = 0; i < N->size; i++) {
+        edge* existing_n = N->edges[i];
+        if (strcmp(n->node, existing_n->node)) {
+            free(n->node);
+            free(n->distance);
+            free(n);
+            N->edges[i] = &DELETED_EDGE;
         }
     }
-    free(E->neighbours);
-    free(E->node);
-    free(E);
 }
 
-void delete_nodes(nodes_table* N) {
+void delete_edges(edges* N) {
+    for (int i = 0; i < N->size; i++) {
+        edge* n = N->edges[i];
+        if (n != NULL && n != &DELETED_EDGE) {
+            delete_edge(N, n);
+        }
+    }
+    free(N->edges);
+    free(N);
+}
+
+
+void delete_edges_table(edges* E) {
+    for (int i = 0; i < E->size; i++) {
+        edges* N = E->edges;
+        delete_edges(N);
+    }
+    free(E->edges);
+    free(E->)
+}
+
+void delete_nodes(nodes* N) {
     for (int i = 0; i < N->size; i++) {
         node* n = N->nodes[i];
         if (n != NULL && n != &DELETED_NODE) {
@@ -235,31 +280,31 @@ void delete_nodes(nodes_table* N) {
 // inaccessible. To handle this, the previous item can be deleted
 // and the new item inserted in its place.
 // To perform resizing, check load on hash table during inserts and deletes.
-void add_edge(edges_table* E, neighbour* n) {
+void add_edge(edges* E, edge* n) {
     const int load = E->count * 100 / E->size;
     if (load > 70) {
         resize_edges(E, 1);
     }
     int index = ht_hash(n->node, E->size, 0);
-    neighbours* cur_neighbour = E->neighbours[index];
+    edges* cur_edge = E->edges[index];
     int i = 1;
-    while(cur_neighbour != NULL) {
-        if (cur_neighbour != &DELETED_NEIGHBOUR) {
-            if (strcmp(cur_neighbour->node, n->node) == 0) {
-                delete_neighbour(cur_neighbour);
-                E->neighbours[index] = n;
+    while(cur_edge != NULL) {
+        if (cur_edge != &DELETED_EDGE) {
+            if (strcmp(cur_edge->node, n->node) == 0) {
+                delete_edge(cur_edge);
+                E->edges[index] = n;
                 return;
             }
         }
         index = ht_hash(n->node, E->size, i);
-        cur_neighbour = E->neighbours[index];
+        cur_edge = E->edges[index];
         i++;
     }
-    E->neighbours[index] = n;
+    E->edges[index] = n;
     E->count++;
 }
 
-void add_node(nodes_table* N, node* n) {
+void add_node(nodes* N, node* n) {
     const int load = N->count * 100 / N->size;
     if (load > 70) {
         resize_nodes(N, 1);
@@ -288,7 +333,7 @@ void add_node(nodes_table* N, node* n) {
 // the key of interest and return the value if found. If the `while` loop
 // reaches a `NULL` value then return `NULL` indicating that the item
 // was not found. Ignore and jump over item marked as deleted.
-node* find_node(nodes_table* N, const char* key) {
+node* find_node(nodes* N, const char* key) {
     int index = ht_hash(key, N->size, 0);
     node* n = N->nodes[index];
     int i = 1;
@@ -305,7 +350,7 @@ node* find_node(nodes_table* N, const char* key) {
     return NULL;
 }
 
-neighbour** find_neighbours(edges_table* E, const char* key) {
+edge** find_edges(edges* E, const char* key) {
     int index = ht_hash(key, E->size, 0);
-    neighbour** n = E->neighbours[index];
+    edge** n = E->edges[index];
 }
